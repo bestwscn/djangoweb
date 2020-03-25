1c0a260fbb3ba78239a38afc8c6d0962281799a7
yaochenfeng <282696845@qq.com>
2020-03-25 21:41:24 +0800

    update https

--- djangoweb/settings.py
+++ djangoweb/settings.py
@@ -95,7 +95,7 @@ TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
+DEFAULT_HTTP_PROTOCOL = "https"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = env('STATIC_URL', default='/static/')
